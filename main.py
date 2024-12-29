bdb5b57c4a20518e00266602c12e0ee601c54b45
Oliver Rojas <oliver.rp2003@gmail.com>
2024-12-29 18:27:49 -0500

    Resquest

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app =  Flask(__name__)
 
 # primer end point con primera ruta
 @app.route("/home") # decorador
 def hello():
-    return "Hello world"
+    user_ip_information = request.remote_addr
+    return f"Hola, como estâs \n tu direcciôn IP es la siguiente: {user_ip_information}"
 
 app.run(host='0.0.0.0', port = 3000, debug = True)
