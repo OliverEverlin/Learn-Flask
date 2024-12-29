3b5b0cd437a564530b4bf5ff150c5b7d8460cb93
Oliver Rojas <oliver.rp2003@gmail.com>
2024-12-29 18:43:37 -0500

    Response&Resquest interaction

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request,  make_response, redirect
 
 app =  Flask(__name__)
 
 # primer end point con primera ruta
-@app.route("/home") # decorador
-def hello():
+@app.route("/index") # decorador
+def index():
     user_ip_information = request.remote_addr
-    return f"Hola, como estâs \n tu direcciôn IP es la siguiente: {user_ip_information}"
+    response = make_response(redirect("/show_information_address"))
+    response.set_cookie("user_ip", user_ip_information)
+    return response
+
+@app.route("/show_information_address")
+def show_information():
+    user_ip = request.cookies.get("user_ip")
+    return f"Tu IP es: {user_ip}"
 
 app.run(host='0.0.0.0', port = 3000, debug = True)
