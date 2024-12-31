09e1201b6ad4a8808f71f5dc56fd8b070c0318c0
Oliver Rojas <oliver.rp2003@gmail.com>
2024-12-31 11:10:35 -0500

    GET&POST methods

--- README.md
+++ README.md
@@ -19,10 +19,6 @@ Este repositorio sirve como documentación personal de mi aprendizaje en el curs
 
 Este repositorio es principalmente para mi uso personal, pero si encuentras algún error o tienes alguna sugerencia, no dudes en abrir un issue.
 
-**Licencia:**
-
-[Especifica la licencia que deseas utilizar, por ejemplo, MIT]
-
 **Agradecimientos:**
 
 Agradezco al creador del curso Lambda Coding por compartir sus conocimientos.
--- main.py
+++ main.py
@@ -26,15 +26,21 @@ def index():
     session["user_ip_information"] = user_ip_information
     return response
 
-@app.route("/show_information_address")
+@app.route("/show_information_address", methods=["GET", "POST"])
 def show_information():
     user_ip = session.get("user_ip_information")
+    username = session.get("username")
     login_form = LoginForm()
     context = {
         "user_ip": user_ip,
         "items": items,
-        "login_form": login_form
+        "login_form": login_form,
+        "username": username
     }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session["username"] = username
+        return make_response(redirect("/index"))
     return render_template("ip_information.html", **context)
 
 app.run(host='0.0.0.0', port = 3000, debug = True)
