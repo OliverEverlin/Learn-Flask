807300c64e1e398d7cba1db60b7954a9463acf8c
Oliver Rojas <oliver.rp2003@gmail.com>
2024-12-31 10:36:36 -0500

    cookies encriptation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request,  make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+
 app =  Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config["SECRET_KEY"]= "CLAVE SEGURA"
 
 items = ["ITEM A", "ITEM B", "ITEM C", "ITEM X", "ITEM Y", "ITEM Z"]
-
 
 @app.errorhandler(404)
 def not_found_endpoint(error):
@@ -14,12 +15,12 @@ def not_found_endpoint(error):
 def index():
     user_ip_information = request.remote_addr
     response = make_response(redirect("/show_information_address"))
-    response.set_cookie("user_ip", user_ip_information)
+    session["user_ip_information"] = user_ip_information
     return response
 
 @app.route("/show_information_address")
 def show_information():
-    user_ip = request.cookies.get("user_ip")
+    user_ip = session.get("user_ip_information")
     context = {
         "user_ip": user_ip,
         "items": items
